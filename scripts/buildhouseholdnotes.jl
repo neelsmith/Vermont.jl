35ae8f637fc86ed35662664f332a2c8b64bcbe2c
Neel Smith <dnsmith.neel@gmail.com>
2025-03-26 15:39:13 -0400

    Build notes listing people

--- scripts/buildhouseholdnotes.jl
+++ scripts/buildhouseholdnotes.jl
@@ -1,10 +1,10 @@
 using Vermont
 repo = pwd()
-outdir1850 = joinpath(repo, "data", "vault", "households1850")
+outdir1850 = joinpath(repo, "data", "vault", "VT-households-1850")
 if !isdir(outdir1850)
     mkpath(outdir1850)
 end
-outdir1860 = joinpath(repo, "data", "vault", "households1860")
+outdir1860 = joinpath(repo, "data", "vault", "VT-households-1860")
 if !isdir(outdir1860)
     mkpath(outdir1860)
 end
@@ -13,17 +13,22 @@ c1850 = readcensusdata(:panton1850)
 c1860 = readcensusdata(:panton1860)
 
 
-for i in houseids(c1850)
-    wikiname = "Panton, Addison, Vermont, 1850, household $(tpl.house)"
+for i in householdids(c1850)
+    wikiname = "Panton, Addison, Vermont, 1850, household $(i)"
     fname = wikiname * ".md"
     f = joinpath(outdir1850, fname)
 
     pagelines = [
         "# $(wikiname)","",
         "enumeration::[[Panton, Addison, Vermont]]","",
-        "year::1850"
+        "year::1850",""
     ]
-    
+    for tpl in filter(t -> t.house == i, c1850)
+        push!(pagelines, tpl.name * "\n")
+    end
+
+    # Add lists of names!
+
 
 
 
@@ -34,17 +39,21 @@ for i in houseids(c1850)
 end
 
 
-for tpl in c1860 
-    wikiname = "Panton, Addison, Vermont, 1860, household $(tpl.house)"
+
+for i in householdids(c1860)
+    wikiname = "Panton, Addison, Vermont, 1860, household $(i)"
     fname = wikiname * ".md"
     f = joinpath(outdir1860, fname)
 
     pagelines = [
         "# $(wikiname)","",
         "enumeration::[[Panton, Addison, Vermont]]","",
-        "year::1860"
+        "year::1860",""
     ]
-    
+    for tpl in filter(t -> t.house == i, c1860)
+        push!(pagelines, tpl.name * "\n")
+    end
+
     open(f, "w") do io
         write(f, join(pagelines, "\n"))
     end
