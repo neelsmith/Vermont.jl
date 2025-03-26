ca2b646c43ee23c1c17dcdf4f0f3d9157ac1b9be
Neel Smith <dnsmith.neel@gmail.com>
2025-03-26 12:50:15 -0400

    Build Obsidian notes for census households

--- scripts/buildhouseholdnotes.jl
+++ scripts/buildhouseholdnotes.jl
@@ -1,27 +1,49 @@
+using Vermont
 repo = pwd()
-
-f = joinpath(repo, "data", "qgspoints.cex")
-
-outdir = joinpath(repo,"vault","households1850")
-if ! isdir(outdir)
-    mkpath(outdir)
+outdir1850 = joinpath(repo, "data", "vault", "households1850")
+if !isdir(outdir1850)
+    mkpath(outdir1850)
+end
+outdir1860 = joinpath(repo, "data", "vault", "households1860")
+if !isdir(outdir1860)
+    mkpath(outdir1860)
 end
 
+c1850 = readcensusdata(:panton1850)
+c1860 = readcensusdata(:panton1860)
 
-data = map(readlines(f)[2:end]) do ln
-    cols = split(ln,"|")
-    (x = cols[1], y = cols[2], house1850 = cols[4])
-end
 
-for tpl in filter(t -> ! isempty(t.house1850), data)
-    label = "Panton, Addison, Vermont, 1850, household $(tpl.house1850)"
-    pagelines = ["---", "location: $(tpl.y),$(tpl.x)", "---","",
-    "# ", label, ""
+for tpl in c1850 
+    wikiname = "Panton, Addison, Vermont, 1850, household $(tpl.house)"
+    fname = wikiname * ".md"
+    f = joinpath(outdir1850, fname)
+
+    pagelines = [
+        "# $(wikiname)","",
+        "enumeration::[[Panton, Addison, Vermont]]","",
+        "year::1850"
     ]
-    txt = join(pagelines,"\n")
-
-    fname = replace(label, " " => "_")
-    outfile = joinpath(outdir, fname)
-    @info("Write output to $(outfile)")
     
+    open(f, "w") do io
+        write(f, join(pagelines, "\n"))
+    end
+    @info("Wrote file $(f)")
+end
+
+
+for tpl in c1860 
+    wikiname = "Panton, Addison, Vermont, 1860, household $(tpl.house)"
+    fname = wikiname * ".md"
+    f = joinpath(outdir1860, fname)
+
+    pagelines = [
+        "# $(wikiname)","",
+        "enumeration::[[Panton, Addison, Vermont]]","",
+        "year::1860"
+    ]
+    
+    open(f, "w") do io
+        write(f, join(pagelines, "\n"))
+    end
+    @info("Wrote file $(f)")
 end
